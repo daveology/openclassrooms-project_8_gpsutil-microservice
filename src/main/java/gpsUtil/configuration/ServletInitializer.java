7842f863a2f5b5958a724aec4a51d458dbe51f11
David <d6797125@pm.me>
2021-12-05 18:34:20 +0100

    refacto: move ServletInitializer to configuration

--- src/main/java/gpsUtil/configuration/ServletInitializer.java
+++ src/main/java/gpsUtil/configuration/ServletInitializer.java
@@ -1,5 +1,6 @@
-package gpsUtil;
+package gpsUtil.configuration;
 
+import gpsUtil.GpsUtilApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
