641f5513e02540459af91a0b04205b9fbe327ddf
David <d6797125@pm.me>
2021-12-05 18:09:07 +0100

    refacto: update domain root name to gpsUtil

--- src/main/java/gpsUtil/ServletInitializer.java
+++ src/main/java/gpsUtil/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.tourguide.gpsutil;
+package gpsUtil;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/test/java/gpsUtil/GpsUtilApplicationTests.java
+++ src/test/java/gpsUtil/GpsUtilApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tourguide.gpsutil;
+package gpsUtil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
