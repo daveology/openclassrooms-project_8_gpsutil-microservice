007e1045d9cbabe830a205b53cf418e44f226604
David <d6797125@pm.me>
2021-12-14 11:50:22 +0100

    feat: add logger system

--- src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
+++ src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
@@ -1,9 +1,15 @@
 package com.tourguide.gpsutil.controller;
 
+import gpsUtil.GpsUtil;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 public class GpsUtilController {
+
+    private Logger logger = LogManager.getLogger(GpsUtilController.class);
+    GpsUtil gpsUtil = new GpsUtil();
     
     
 }
