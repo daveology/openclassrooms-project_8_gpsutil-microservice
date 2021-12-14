e06300cfbe235b8766e13186d63b327084a56460
David <d6797125@pm.me>
2021-12-14 11:55:15 +0100

    feat: add getAttractions method from gpsUtil.jar

--- src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
+++ src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
@@ -1,15 +1,23 @@
 package com.tourguide.gpsutil.controller;
 
 import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 @RestController
 public class GpsUtilController {
 
     private Logger logger = LogManager.getLogger(GpsUtilController.class);
     GpsUtil gpsUtil = new GpsUtil();
-    
-    
+
+    public List<Attraction> getAttractions() {
+
+        logger.debug("Attractions received!");
+
+        return gpsUtil.getAttractions();
+    }
 }
