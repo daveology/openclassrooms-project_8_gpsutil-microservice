181f751c0b3836ed64587bca4ee11498c52c0c46
David <d6797125@pm.me>
2021-12-15 09:36:49 +0100

    build: removes unnecessary imports

--- src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
+++ src/main/java/com/tourguide/gpsutil/controller/GpsUtilController.java
@@ -1,15 +1,12 @@
 package com.tourguide.gpsutil.controller;
 
 import com.tourguide.gpsutil.service.GpsUtilService;
-import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.UUID;
