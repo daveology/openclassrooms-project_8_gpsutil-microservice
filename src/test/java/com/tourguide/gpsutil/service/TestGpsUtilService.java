2de40a622e9b675dcb79e11bd9d6e9e74a5db358
David <d6797125@pm.me>
2021-12-18 20:03:28 +0100

    test(service): add shouldUserLocation unit test

--- src/test/java/com/tourguide/gpsutil/service/TestGpsUtilService.java
+++ src/test/java/com/tourguide/gpsutil/service/TestGpsUtilService.java
@@ -1,11 +1,13 @@
 package com.tourguide.gpsutil.service;
 
 import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +23,13 @@ public class TestGpsUtilService {
         List<Attraction> attractions = gpsUtilService.getAttractions();
 
         assertEquals(26, attractions.size());
+    }
+
+    @Test
+    public void shouldUserLocation() {
+
+        VisitedLocation visitedLocation = gpsUtilService.getUserLocation(UUID.randomUUID());
+
+        assertEquals(true, !visitedLocation.equals(null));
     }
 }
