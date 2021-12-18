55fa575ddb9d23968f5b4d5358a5bd258546a07f
David <d6797125@pm.me>
2021-12-18 19:59:02 +0100

    test: add TestGpsUtilService

---
+++ src/test/java/com/tourguide/gpsutil/service/TestGpsUtilService.java
@@ -0,0 +1,11 @@
+package com.tourguide.gpsutil.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestGpsUtilService {
+
+    @Autowired
+    GpsUtilService gpsUtilService;
+}
