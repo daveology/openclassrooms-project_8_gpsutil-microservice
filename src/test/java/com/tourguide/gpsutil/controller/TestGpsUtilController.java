c3d46ac68f10ddc36b15e8b5c3b9c4ed1e3eec69
David <d6797125@pm.me>
2021-12-18 19:52:05 +0100

    test(controller): add shouldAccessAttractions unit test

--- src/test/java/com/tourguide/gpsutil/controller/TestGpsUtilController.java
+++ src/test/java/com/tourguide/gpsutil/controller/TestGpsUtilController.java
@@ -6,10 +6,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class TestGpsUtilController {
 
     @Autowired
     private MockMvc mock;
+
+    @Test
+    public void shouldAccessAttractions() throws Exception {
+
+        mock.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(26)));
+    }
 }
