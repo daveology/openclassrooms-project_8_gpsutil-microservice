a7d5a3679d06a75658cbef64fddb8d1e2cfa71aa
David <d6797125@pm.me>
2021-12-18 20:01:32 +0100

    test(service): add shouldGetAttractions unit test

--- src/test/java/com/tourguide/gpsutil/service/TestGpsUtilService.java
+++ src/test/java/com/tourguide/gpsutil/service/TestGpsUtilService.java
@@ -1,11 +1,25 @@
 package com.tourguide.gpsutil.service;
 
+import gpsUtil.location.Attraction;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TestGpsUtilService {
 
     @Autowired
     GpsUtilService gpsUtilService;
+
+    @Test
+    public void shouldGetAttractions() {
+
+        List<Attraction> attractions = gpsUtilService.getAttractions();
+
+        assertEquals(26, attractions.size());
+    }
 }
